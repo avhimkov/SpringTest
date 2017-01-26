28efe7a5810bfed00aa2e426798b32a177191b56
avhimkov <avhimkov@mail.ru>
2017-01-26 16:21:22 +0500

    refactoring Main class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        MessageRenderer mr = new StandardOutMessageRenderer();
-        MessageProvider mp = new HelloWorlMessageProvider();
+        MessageRenderer mr = MessageSupportFactory.getInstance().getMessageRenderer();
+        MessageProvider mp = MessageSupportFactory.getInstance().getMessageProvider();
         mr.setMessageProvider(mp);
         mr.render();
     }
