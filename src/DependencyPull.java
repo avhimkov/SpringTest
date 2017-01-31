0c3959e0064fb8d70471778f0601a889daa2c4e8
avhimkov <avhimkov@mail.ru>
2017-01-31 22:52:54 +0500

    add ManagedComponent interface

--- src/DependencyPull.java
+++ src/DependencyPull.java
@@ -1,11 +1,6 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.applet.AppletContext;
-
-/**
- * Created by adl on 31.01.2017.
- */
 public class DependencyPull {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
---
+++ src/ManagedComponent.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+/**
+ * Created by adl on 31.01.2017.
+ */
+public interface ManagedComponent {
+    void performLookup (Container container);
+}
