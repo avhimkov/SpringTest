f6b1d27047004ea9857a25b7c2870894b1f9f32c
avhimkov <avhimkov@mail.ru>
2017-02-01 18:53:18 +0500

    add class Bookworm

---
+++ src/BookwormOracle.java
@@ -0,0 +1,14 @@
+/**
+ * Created by adl on 01.02.17.
+ */
+public class BookwormOracle implements Oracle {
+    private Encyclopedia encyclopedia;
+    public void setEncyclopedia (Encyclopedia encyclopedia ){
+        this.encyclopedia= encyclopedia;
+    }
+
+    @Override
+    public String defineMeaningOfLife (){
+        return "Encyclopedias are a waste of money - use the Internet";
+    }
+}
