ef907f50626c0a2d7123003eb48adff5a7f2c0a4
avhimkov <avhimkov@mail.ru>
2017-02-01 18:58:47 +0500

    add NewsLetterSender

---
+++ src/NewsletterSender.java
@@ -0,0 +1,10 @@
+/**
+ * Created by adl on 01.02.17.
+ */
+public interface NewsletterSender {
+    void setSmtpServer(String smtpServer);
+    String getSmtServer();
+    void setFromAddress(String fromAddress);
+    String getFromeAddress();
+    void send();
+}
