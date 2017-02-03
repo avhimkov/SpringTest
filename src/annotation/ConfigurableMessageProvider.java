16063e1bc329d70c6c206008ef1c8395ba43ca7f
avhimkov <avhimkov@mail.ru>
2017-02-03 19:02:16 +0500

    annotation ConfigurableMessageProvider class

---
+++ src/annotation/ConfigurableMessageProvider.java
@@ -0,0 +1,19 @@
+package annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service("messageProvider")
+public class ConfigurableMessageProvider implements MessageProvider {
+    private String message;
+
+    @Autowired
+    public ConfigurableMessageProvider(String message){
+        this.message = message;
+    }
+    @Override
+    public String getMessage(){
+        return message;
+    }
+}
