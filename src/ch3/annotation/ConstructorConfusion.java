d08eadbc03272e54596e94517b77586277cb1ff1
avhimkov <avhimkov@mail.ru>
2017-02-07 15:13:50 +0500

    fix ConstructorConfusion class

--- src/ch3/annotation/ConstructorConfusion.java
+++ src/ch3/annotation/ConstructorConfusion.java
@@ -1,13 +1,17 @@
 package ch3.annotation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Service;
 
+@Service ("constructorConfusion")
 public class ConstructorConfusion {
     private String someValue;
     public ConstructorConfusion(String someValue){
         System.out.println("ConstructorConfusion(String) called");
         this.someValue = someValue;
     }
+    @Autowired
     public ConstructorConfusion(int someValue){
         System.out.println("ConstructorConfusion(int) called");
         this.someValue = "Number: " + Integer.toString(someValue);
