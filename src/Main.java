176cf8ea95fee5b55057a6ef150191f39d0833de
Dmitry <avhimkov@mail.ru>
2017-02-09 20:44:20 +0500

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        MessageRenderer mr = MessageSupportFactory.getInstance().getMessageRenderer();
-        MessageProvider mp = MessageSupportFactory.getInstance().getMessageProvider();
-        mr.setMessageProvider(mp);
-        mr.render();
-    }
-}
